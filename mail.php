e7dbecf90ad545c8316541e3f99f7fda73272140
radek-admin <120376621+radek-admin@users.noreply.github.com>
2023-03-15 10:12:41 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,30 +1,4 @@
 <?php
 
-
-$first_name = $_POST['first-name'];
-$last_name = $_POST['last-name'];
-
-$email = $_POST['email'];
-$website = $_POST['website'];
-$message = $_POST['message'];
-
-$dokogo = "dawid@wykreowani.pl";
-$tytul = "Formularz kontaktowy - futurum";
-
-$wiadomosc = "";
-$wiadomosc .= "Imię: " . $first_name . "\n \n";
-$wiadomosc .= "Nazwisko: " . $last_name . "\n \n";
-$wiadomosc .= "Email: " . $email . "\n \n";
-$wiadomosc .= "Website: " . $website . "\n \n";
-$wiadomosc .= "Wiadomość: \n" . $message . "\n \n";
-
-$naglowek = "";
-$naglowek .= "Od:" . $odkogo . " \n";
-$naglowek .= "Content-Type:text/plain;charset=utf-8";
-
-$sukces = mail($dokogo, $tytul, $wiadomosc, $naglowek);
-
-if ($sukces){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
-}
+echo 'dziala';
 ?>
